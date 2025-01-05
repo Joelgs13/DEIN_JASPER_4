b22bbdc8cf0620fe54f5e275b0f9aa4df021542f
Joelgs13 <050213jgonzalez@gmail.com>
2025-01-05 14:01:55 +0100

    interfaz realizada

---
+++ src/main/java/joel/dein/ejercicio4_jasper/InterfazController.java
@@ -0,0 +1,56 @@
+package joel.dein.ejercicio4_jasper;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class InterfazController {
+
+    @FXML
+    private Button btGenerar;
+
+    @FXML
+    private Button btLimpiar;
+
+    @FXML
+    private Button btSalir;
+
+    @FXML
+    private TextArea txaTratamiento;
+
+    @FXML
+    private TextField txtCodMedico;
+
+    @FXML
+    private TextField txtDirPaciente;
+
+    @FXML
+    private TextField txtEspMedico;
+
+    @FXML
+    private TextField txtNomMedico;
+
+    @FXML
+    private TextField txtNomPaciente;
+
+    @FXML
+    private TextField txtNumPaciente;
+
+    @FXML
+    void generarInforme(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiarFormulario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salir(ActionEvent event) {
+
+    }
+
+}
